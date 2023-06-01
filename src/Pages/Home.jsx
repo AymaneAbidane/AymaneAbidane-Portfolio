3a3ac57959dade58e06920ff49023c9b67f5f0e8
AymaneAbidane <heymane1997@gmail.com>
2023-06-01 18:53:30 +0100

    adding project to teh deployment

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,13 +4,14 @@ import { Banner } from '../Components/Banner'
 import { Skills } from '../Components/Skills'
 import { Contact } from '../Components/Contact'
 import { Footer } from '../Components/Footer'
-
+import {Projects} from '../Components/Projects'
 export const Home = () => {
   return (
     <>
     <NavBar/>
     <Banner/>
     <Skills/>
+    <Projects/>
     <Contact/>
     <Footer/>
     </>
